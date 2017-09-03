5321d790eb9085f45cc02f80b6ce55c417a5fe40
André Lucas <andrelucastic@gmail.com>
2017-09-03 16:35:42 -0300

    new serve

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,3 +1,3 @@
-export const CAPSRNRB_HOST = 'http://ec2-54-236-23-178.compute-1.amazonaws.com:8080/capsrnrb/'
+export const CAPSRNRB_HOST = 'http://165.227.176.97:8080/capsrnrb/'
 export const CLIENT_ID = 'capsrnrbid'
 export const CLIENT_SECRET = 's3cr3t_rnrb'
