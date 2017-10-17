eac3a8c270c44773b39d954a86af1d2cb5ba8e72
Andre Lucas <andresantos@andre.santos>
2017-10-17 12:41:52 -0300

    host modify

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,3 +1,3 @@
-export const CAPSRNRB_HOST = 'http://165.227.176.97:8080/capsrnrb/'
+export const CAPSRNRB_HOST = 'http://localhost:8080/capsrnrb/'
 export const CLIENT_ID = 'capsrnrbid'
 export const CLIENT_SECRET = 's3cr3t_rnrb'
