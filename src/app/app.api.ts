1a631e547239f087c7db9c282e2462a43b32c1ea
andrelucas <andrelucastic@gmail.com>
2017-10-22 11:06:11 -0300

    Floating IP

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,3 +1,3 @@
-export const CAPSRNRB_HOST = 'http://104.131.116.91:8080/capsrnrb'
+export const CAPSRNRB_HOST = 'http://165.227.249.123:8080/capsrnrb'
 export const CLIENT_ID = 'capsrnrbid'
 export const CLIENT_SECRET = 's3cr3t_rnrb'
